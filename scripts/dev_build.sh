e777fa49be7859860935a63e8fb488307aab141b
aravindcloud01 <127280225+aravindcloud01@users.noreply.github.com>
2023-06-29 20:56:40 +0530

    Update dev_build.sh

--- scripts/dev_build.sh
+++ scripts/dev_build.sh
@@ -3,4 +3,4 @@ IMAGENAME="reactapp_dev"
 CONTAINER_NAME="reactapp-dev"
 docker stop $CONTAINER_NAME
 docker rmi -f $(docker images -aq)
-docker build -f Dockerfile.dev -t reactapp_dev .
+docker build -f ../Dockerfile.dev -t reactapp_dev .
