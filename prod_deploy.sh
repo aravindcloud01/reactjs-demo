437929a86dd568704cd1a7c12f6e37beffff2d4f
aravindcloud01 <127280225+aravindcloud01@users.noreply.github.com>
2023-06-30 07:08:14 +0530

    Update prod_deploy.sh

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -3,4 +3,4 @@ CONTAINER_NAME="reactapp-prod"
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME -f
 
-docker compose -f /opt/reactjs-demo/docker-compose.prod.yaml up -d
+docker compose -f docker-compose.prod.yaml up -d
